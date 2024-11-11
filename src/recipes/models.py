f72ccb618980e558eaddb1cf1ecb3b3b4b0f8cc9
Vinnie <vincentathorne2005@gmail.com>
2024-11-11 14:15:04 -0800

    problem

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -13,6 +13,7 @@ class Recipe(models.Model):
   ingredients = models.CharField(max_length=225)
   cooking_time = models.IntegerField(default=10)
   difficulty = models.CharField(max_length=30, choices=difficulty)
+  pic = models.ImageField(upload_to='recipes', default='no_image.png')
 
   def __str__(self): 
     return str(self.name)
