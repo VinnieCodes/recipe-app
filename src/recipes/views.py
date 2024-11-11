e802ce086e5a85aeda933b6379b05f5f7854f80b
Vinnie <vincentathorne2005@gmail.com>
2024-11-11 15:17:11 -0800

    link not working

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render                     #imported by default
+from django.views.generic import ListView, DetailView   #to display lists and details
+from .models import Recipe                                #to access Book model
 
 # Create your views here.
+class RecipeListView(ListView):                           #class-based view
+  model = Recipe                                        #specify model
+  template_name = 'recipes/main.html'                   #specify template
 
-def home(request):
-  return render(request, 'recipes/recipes_home.html')
+class RecipeDetailView(DetailView):                       #class-based view
+  model = Recipe                                        #specify model
+  template_name = 'recipes/detail.html'                 #specify template
