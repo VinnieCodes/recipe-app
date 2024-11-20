36c59f807761fa25d78e5626365a6f29de05774e
Vinnie <vincentathorne2005@gmail.com>
2024-11-20 14:06:39 -0800

    not sure where to put @login_required

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render                     #imported by default
 from django.views.generic import ListView, DetailView, TemplateView   #to display lists and details
 from .models import Recipe                                #to access Book model
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-class RecipeListView(ListView):                           #class-based view
+class RecipeListView(LoginRequiredMixin, ListView):                           #class-based view
   model = Recipe                                        #specify model
   template_name = 'recipes/main.html'                   #specify template
 
-class RecipeDetailView(DetailView):                       #class-based view
+class RecipeDetailView(LoginRequiredMixin, DetailView):                       #class-based view
   model = Recipe                                        #specify model
   template_name = 'recipes/detail.html'                 #specify template
 
--- src/views.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import reverse
-
-def login_view(request):
-    # ...existing code...
-    return redirect(reverse('recipes:index'))
-    # ...existing code...
