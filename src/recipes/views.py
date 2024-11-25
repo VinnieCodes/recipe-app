050f3c54a4d33a53b3c2c0b325db6f98bd2f2150
Vinnie <vincentathorne2005@gmail.com>
2024-11-25 14:51:25 -0800

    2.6

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -3,11 +3,11 @@ from django.views.generic import ListView, DetailView, TemplateView   #to displa
 from .models import Recipe                                #to access Book model
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 class RecipeListView(LoginRequiredMixin, ListView):                           #class-based view
   model = Recipe                                        #specify model
   template_name = 'recipes/main.html'                   #specify template
-
 class RecipeDetailView(LoginRequiredMixin, DetailView):                       #class-based view
   model = Recipe                                        #specify model
   template_name = 'recipes/detail.html'                 #specify template
